ad58b0dfd4dbf5b4db92316b8fb600026c9ba3cb
t.akasaki <t.akasaki.csharp@gmail.com>
2020-12-12 13:38:07 +0900

    fix(lib): :bug: expectにextendするように修正

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,12 @@ export * from './ToBeMethodDefied';
 export * from './ToBeMethodFunction';
 export * from './ToBeMethodValidation';
 
-//
+import toBeMethodAuthentication from './ToBeMethodAuthentication';
+import toBeMethodDefied from './ToBeMethodDefied';
+import toBeMethodFunction from './ToBeMethodFunction';
+import toBeMethodValidation from './ToBeMethodValidation';
+
+expect.extend(toBeMethodAuthentication);
+expect.extend(toBeMethodFunction);
+expect.extend(toBeMethodValidation);
+expect.extend(toBeMethodDefied);
